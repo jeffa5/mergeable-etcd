da474f13246aaa971a71df8b41bd9a8725072ec5
Andrew Jeffery <dev@jeffas.io>
2021-01-28 14:56:55 +0000

    Add building with nix

--- flake.nix
+++ flake.nix
@@ -4,28 +4,69 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+    naersk.url = "github:nmattia/naersk";
   };
 
-  outputs = { self, nixpkgs, rust-overlay }:
-    with import nixpkgs { overlays = [ rust-overlay.overlay ]; system = "x86_64-linux"; };
-    {
-      devShell.x86_64-linux = mkShell {
-        buildInputs = [
-          (rust-bin.nightly.latest.rust.override {
-            extensions = [ "rust-src" ];
-          })
-          cargo-edit
-          cargo-watch
-          protobuf
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, naersk }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = import nixpkgs
+            {
+              overlays = [ rust-overlay.overlay ];
+              system = system;
+            };
+          rust = pkgs.rust-bin.nightly.latest.rust;
+          naersk-lib = naersk.lib."${system}".override {
+            cargo = rust;
+            rustc = rust;
+          };
+        in
+        rec
+        {
+          packages = {
+            eckd = naersk-lib.buildPackage {
+              doCheck = true;
+              pname = "eckd";
+              root = ./.;
+            };
 
-          etcd
+            eckd-docker = pkgs.dockerTools.buildImage {
+              name = "eckd-rs";
+              tag = "latest";
+              contents = packages.eckd;
 
-          rnix-lsp
-          nixpkgs-fmt
-        ];
+              config.Cmd = [ "/bin/eckd" ];
+            };
+          };
 
-        ETCDCTL_API = 3;
-        PROTOC = "protoc";
-      };
-    };
+          defaultPackage = packages.eckd;
+
+          apps.eckd = flake-utils.lib.mkApp {
+            drv = packages.eckd;
+          };
+
+          defaultApp = apps.eckd;
+
+          devShell = pkgs.mkShell {
+            buildInputs = with pkgs;[
+              (rust.override {
+                extensions = [ "rust-src" ];
+              })
+              cargo-edit
+              cargo-watch
+              protobuf
+
+              etcd
+
+              rnix-lsp
+              nixpkgs-fmt
+            ];
+
+            ETCDCTL_API = 3;
+            PROTOC = "protoc";
+          };
+        }
+      );
 }
