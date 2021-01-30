6722304043d7578dc0e0ca7f7fdc3ee66936dc6f
Andrew Jeffery <dev@jeffas.io>
2021-01-30 13:47:02 +0000

    Add more options

--- eckd/src/main.rs
+++ eckd/src/main.rs
@@ -26,6 +26,10 @@ struct Options {
     #[structopt(long, parse(try_from_str = Address::try_from), default_value = "http://localhost:2379", use_delimiter=true)]
     listen_client_urls: Vec<Address>,
 
+    /// List of URLs to listen on for metrics.
+    #[structopt(long, parse(try_from_str = Address::try_from))]
+    listen_metrics_urls: Vec<Address>,
+
     /// List of this member's peer URLs to advertise to the rest of the cluster.
     #[structopt(long, parse(try_from_str = Address::try_from), default_value = "http://localhost:2380", use_delimiter=true)]
     initial_advertise_peer_urls: Vec<Address>,
@@ -39,6 +43,42 @@ struct Options {
     /// The client URLs advertised should be accessible to machines that talk to etcd cluster. etcd client libraries parse these URLs to connect to the cluster.
     #[structopt(long, parse(try_from_str = Address::try_from), default_value = "http://localhost:2379", use_delimiter=true)]
     advertise_client_urls: Vec<Address>,
+
+    /// Path to the client server TLS cert file.
+    #[structopt(long)]
+    cert_file: PathBuf,
+
+    /// Path to the client server TLS key file.
+    #[structopt(long)]
+    key_file : PathBuf,
+
+    /// Path to the client server TLS trusted CA cert file.
+    #[structopt(long)]
+    trusted_ca_file:PathBuf,
+
+    /// Enable client cert authentication.
+    #[structopt(long)]
+    client_cert_auth: bool,
+
+    /// Path to the peer server TLS cert file.
+    #[structopt(long)]
+    peer_cert_file: PathBuf,
+
+    /// Path to the peer server TLS key file.
+    #[structopt(long)]
+    peer_key_file: PathBuf,
+
+    /// Enable peer client cert authentication.
+    #[structopt(long)]
+    peer_client_cert_auth: bool,
+
+    /// Path to the peer server TLS trusted CA file.
+    #[structopt(long)]
+    peer_trusted_ca_file: PathBuf,
+
+    /// Number of committed transactions to trigger a snapshot to disk.
+    #[structopt(long)]
+    snapshot_count:usize,
 }
 
 #[tokio::main]
