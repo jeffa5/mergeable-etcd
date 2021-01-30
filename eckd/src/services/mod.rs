5902b3899222e2d192d187b9a4f6a386cf0c8f0b
Andrew Jeffery <dev@jeffas.io>
2021-01-30 15:43:12 +0000

    Add catchall service for logging

--- eckd/src/services/mod.rs
+++ eckd/src/services/mod.rs
@@ -1,9 +1,16 @@
 mod kv;
-
-use std::net::SocketAddr;
+use std::{
+    net::SocketAddr,
+    task::{Context, Poll},
+};
 
 use etcd_proto::etcdserverpb::kv_server::KvServer;
-use tonic::transport::{Identity, Server, ServerTlsConfig};
+use hyper::{Body, Request as HyperRequest, Response as HyperResponse};
+use tonic::{
+    body::BoxBody,
+    transport::{Identity, NamedService, Server, ServerTlsConfig},
+};
+use tower::Service;
 
 pub async fn serve(
     address: SocketAddr,
@@ -17,5 +24,33 @@ pub async fn serve(
         server = server.tls_config(ServerTlsConfig::new().identity(identity))?;
     }
 
-    server.add_service(kv_service).serve(address).await
+    server
+        .add_service(kv_service)
+        .add_service(CatchAllService {})
+        .serve(address)
+        .await
+}
+
+#[derive(Debug, Clone)]
+struct CatchAllService {}
+
+impl Service<HyperRequest<Body>> for CatchAllService {
+    type Response = hyper::Response<BoxBody>;
+    type Error = hyper::http::Error;
+    type Future = futures::future::BoxFuture<'static, Result<Self::Response, Self::Error>>;
+
+    fn poll_ready(&mut self, _cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        Poll::Ready(Ok(()))
+    }
+
+    fn call(&mut self, req: HyperRequest<Body>) -> Self::Future {
+        Box::pin(async move {
+            println!("{:?} {:?}", req.method(), req.uri());
+            HyperResponse::builder().status(404).body(BoxBody::empty())
+        })
+    }
+}
+
+impl NamedService for CatchAllService {
+    const NAME: &'static str = "";
 }
