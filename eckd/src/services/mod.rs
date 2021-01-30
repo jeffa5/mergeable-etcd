06991cd24170f4ba901065ebb1b69175b4a11d96
Andrew Jeffery <dev@jeffas.io>
2021-01-30 12:59:32 +0000

    Tidy up kv service

--- eckd/src/services/mod.rs
+++ eckd/src/services/mod.rs
@@ -3,14 +3,16 @@ mod kv;
 use std::net::SocketAddr;
 
 use tonic::transport::Server;
+use etcd_proto::etcdserverpb::kv_server::KvServer;
 
 pub async fn serve(
     address: SocketAddr,
     db: &crate::store::Db,
 ) -> Result<(), tonic::transport::Error> {
     let kv = kv::KV::new(db);
+    let kv_service = KvServer::new(kv);
     Server::builder()
-        .add_service(etcd_proto::etcdserverpb::kv_server::KvServer::new(kv))
+        .add_service(kv_service)
         .serve(address)
         .await
 }
