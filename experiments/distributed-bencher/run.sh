53080c3f202b23e0f0c4d09eb690578d22bb747f
Andrew Jeffery <dev@jeffas.io>
2021-10-06 17:45:59 +0100

    Fix binary_name

--- experiments/distributed-bencher/run.sh
+++ experiments/distributed-bencher/run.sh
@@ -29,7 +29,7 @@ max_node_count=${max_node_count:-5}
 
 for node_count in $(seq 1 2 $max_node_count); do
   for node_image in "quay.io/coreos/etcd" "jeffas/recetcd:latest"; do
-    binary_name="$(echo $node_image | cut -d '/' -f 3 | cut -d ':' -f 1)"
+    binary_name="$(echo $node_image | rev | cut -d '/' -f 1 | rev | cut -d ':' -f 1)"
     for repeat in $(seq 1 $num_repeats); do
       echo "Running with node_count=$node_count node_image=$node_image binary_name=$binary_name repeat=$repeat"
       ansible-playbook main.yaml -e @values.yaml -e node_count="$node_count" -e node_image="$node_image" -e binary_name="$binary_name" -e repeat="$repeat"
