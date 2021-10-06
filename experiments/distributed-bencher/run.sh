59bb33687e3dc01f55c51f3c0d7a9fb69390093f
Andrew Jeffery <dev@jeffas.io>
2021-10-06 17:32:24 +0100

    Run repeats and create the run directory

--- experiments/distributed-bencher/run.sh
+++ experiments/distributed-bencher/run.sh
@@ -1,10 +1,38 @@
 #!/usr/bin/env bash
 
-max_node_count=${1:-5}
+function usage() {
+  echo "$0 [-r <repeats>] [-s <size>]"
+}
+
+while getopts "r:s:h" option; do
+  case ${option} in
+    r )
+    num_repeats=$OPTARG
+    ;;
+    s )
+    max_node_count=$OPTARG
+    ;;
+    h )
+    usage
+    exit 0
+    ;;
+    \? ) #For invalid option
+    usage
+    exit 1
+    ;;
+  esac
+done
+
+num_repeats=${num_repeats:-3}
+
+max_node_count=${max_node_count:-5}
+
 for node_count in $(seq 1 2 $max_node_count); do
   for node_image in "quay.io/coreos/etcd" "jeffas/recetcd:latest"; do
     binary_name="$(echo $node_image | cut -d '/' -f 3 | cut -d ':' -f 1)"
-    echo "Running with node_count=$node_count node_image=$node_image binary_name=$binary_name"
-    ansible-playbook main.yaml -e @values.yaml -e node_count="$node_count" -e node_image="$node_image" -e binary_name="$binary_name"
+    for repeat in $(seq 1 $num_repeats); do
+      echo "Running with node_count=$node_count node_image=$node_image binary_name=$binary_name repeat=$repeat"
+      ansible-playbook main.yaml -e @values.yaml -e node_count="$node_count" -e node_image="$node_image" -e binary_name="$binary_name" -e repeat="$repeat"
+    done
   done
 done
