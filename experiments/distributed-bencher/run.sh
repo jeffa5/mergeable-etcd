86fcd3031d50533e0e11f92fa49d54b487c2ef21
Andrew Jeffery <dev@jeffas.io>
2021-10-06 16:54:21 +0100

    Add more node counts to run

--- experiments/distributed-bencher/run.sh
+++ experiments/distributed-bencher/run.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-node_count=3
-# for node_count in $(seq 1 2 5); do
-  for node_image in "quay.io/coreos/etcd" "docker.io/jeffas/recetcd:latest"; do
+max_node_count=${1:-5}
+for node_count in $(seq 1 2 $max_node_count); do
+  for node_image in "quay.io/coreos/etcd" "jeffas/recetcd:latest"; do
     binary_name="$(echo $node_image | cut -d '/' -f 3 | cut -d ':' -f 1)"
     echo "Running with node_count=$node_count node_image=$node_image binary_name=$binary_name"
     ansible-playbook main.yaml -e @values.yaml -e node_count="$node_count" -e node_image="$node_image" -e binary_name="$binary_name"
   done
-# done
+done
