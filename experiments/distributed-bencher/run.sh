5f10bbec1e2747b2073ce6b1470a3ed1c22703cc
Andrew Jeffery <dev@jeffas.io>
2021-10-13 09:39:05 +0100

    Add bench_type to play

--- experiments/distributed-bencher/run.sh
+++ experiments/distributed-bencher/run.sh
@@ -5,9 +5,10 @@ set -e
 num_repeats=3
 min_node_count=1
 max_node_count=9
+bencher_subcommand=""
 
 function usage() {
-  echo "$0 [-r <repeats=$num_repeats>] [-b <start_size=$min_node_count>] [-e <end_size=$max_node_count>]"
+  echo "$0 [-r <repeats=$num_repeats>] [-b <start_size=$min_node_count>] [-e <end_size=$max_node_count>] [-c <bencher_subcommand>]"
 }
 
 while getopts "r:b:e:h" option; do
@@ -20,6 +21,9 @@ while getopts "r:b:e:h" option; do
     ;;
     e )
     max_node_count=$OPTARG
+    ;;
+    c )
+    bencher_subcommand=$OPTARG
     ;;
     h )
     usage
@@ -35,16 +39,26 @@ done
 
 function run() {
   node_image=$1
+  subcommand=$2
 
   binary_name="$(echo $node_image | rev | cut -d '/' -f 1 | rev | cut -d ':' -f 1)"
 
   for repeat in $(seq 1 $num_repeats); do
-    echo "Running with node_count=$node_count node_image=$node_image binary_name=$binary_name repeat=$repeat"
-   ansible-playbook main.yaml -e @values.yaml -e node_count="$node_count" -e node_image="$node_image" -e binary_name="$binary_name" -e repeat="$repeat"
+    echo "Running with node_count=$node_count node_image=$node_image binary_name=$binary_name repeat=$repeat bench_type=$subcommand"
+   ansible-playbook main.yaml -e @values.yaml -e node_count="$node_count" -e node_image="$node_image" -e binary_name="$binary_name" -e repeat="$repeat" -e bench_type="$subcommand"
   done
 }
 
 for node_count in $(seq $min_node_count 2 $max_node_count); do
-  run "quay.io/coreos/etcd"
-  run "jeffas/recetcd:latest"
+  if [[ $bencher_subcommand != "" ]]; then
+    run "quay.io/coreos/etcd" $bencher_subcommand
+    run "jeffas/recetcd:latest" $bencher_subcommand
+  else
+    run "quay.io/coreos/etcd" "PutRange"
+    run "jeffas/recetcd:latest" "PutRange"
+    run "quay.io/coreos/etcd" "PutSingle"
+    run "jeffas/recetcd:latest" "PutSingle"
+    run "quay.io/coreos/etcd" "PutRandom"
+    run "jeffas/recetcd:latest" "PutRandom"
+  fi
 done
