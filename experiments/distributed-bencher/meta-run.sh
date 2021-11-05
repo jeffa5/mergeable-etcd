033a677743ca2a8cfa74b990861141bde68abf51
Andrew Jeffery <dev@jeffas.io>
2021-11-05 14:28:58 +0000

    Speed up runs

--- experiments/distributed-bencher/meta-run.sh
+++ experiments/distributed-bencher/meta-run.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 
-while [[ true ]]; do
-    ./run.sh $@
-    if [[ $? -eq 0 ]]; then
-        break
-    else
-        echo "Encountered a failure, re-running"
-        ./teardown.sh
-    fi
+ansible-playbook setup.yaml
+
+for delay in 5 6 7 8 9 10 15 20 50 100 200 400 1000; do
+    while [[ true ]]; do
+        ./run.sh -d $delay $@
+        if [[ $? -eq 0 ]]; then
+            break
+        else
+            echo "Encountered a failure, re-running"
+            ./teardown.sh
+        fi
+    done
 done
--- experiments/distributed-bencher/run.sh
+++ experiments/distributed-bencher/run.sh
@@ -4,14 +4,15 @@ set -e
 
 num_repeats=3
 min_node_count=1
-max_node_count=9
+max_node_count=15
 bencher_subcommand=""
+iterations=1000
 
 function usage() {
-  echo "$0 [-r <repeats=$num_repeats>] [-b <start_size=$min_node_count>] [-e <end_size=$max_node_count>] [-c <bencher_subcommand>] [-d <delay>]"
+  echo "$0 [-r <repeats=$num_repeats>] [-b <start_size=$min_node_count>] [-e <end_size=$max_node_count>] [-c <bencher_subcommand>] [-d <delay>] [-i <iterations>]"
 }
 
-while getopts "r:b:e:c:d:h" option; do
+while getopts "r:b:e:c:d:i:h" option; do
   case ${option} in
   r)
     num_repeats=$OPTARG
@@ -27,6 +28,9 @@ while getopts "r:b:e:c:d:h" option; do
     ;;
   d)
     delay=$OPTARG
+    ;;
+  i)
+    iterations=$OPTARG
     ;;
   h)
     usage
@@ -47,11 +51,11 @@ function run() {
   binary_name="$(echo $node_image | rev | cut -d '/' -f 1 | rev | cut -d ':' -f 1)"
 
   for repeat in $(seq 1 $num_repeats); do
-    run_dir="results/$binary_name-${node_count}nodes-1benchers-${subcommand}-${interval}ms/repeat${repeat}"
+    run_dir="results/$binary_name-${node_count}nodes-1benchers-${subcommand}-${interval}ms-${iterations}iters/repeat${repeat}"
     # only run the playbook if the directory (results) doesn't already exist
     if [[ ! -d $run_dir  ]]; then
         echo "Running with node_count=$node_count node_image=$node_image binary_name=$binary_name repeat=$repeat bench_type=$subcommand interval=$interval"
-        ansible-playbook main.yaml -e @values.yaml -e node_count="$node_count" -e node_image="$node_image" -e binary_name="$binary_name" -e repeat="$repeat" -e bench_type="$subcommand" -e bencher_interval="$interval"
+        ansible-playbook main.yaml -e @values.yaml -e node_count="$node_count" -e node_image="$node_image" -e binary_name="$binary_name" -e repeat="$repeat" -e bench_type="$subcommand" -e bencher_interval="$interval" -e bench_iterations="$iterations"
     else
         echo "Skipping with node_count=$node_count node_image=$node_image binary_name=$binary_name repeat=$repeat bench_type=$subcommand interval=$interval"
     fi
@@ -64,8 +68,8 @@ for node_count in $(seq $min_node_count 2 $max_node_count); do
       run "quay.io/coreos/etcd:v3.4.13" $bencher_subcommand $delay
       run "jeffas/recetcd:latest" $bencher_subcommand $delay
     else
-      run "quay.io/coreos/etcd:v3.4.13" "PutRange" $delay
-      run "jeffas/recetcd:latest" "PutRange" $delay
+      # run "quay.io/coreos/etcd:v3.4.13" "PutRange" $delay
+      # run "jeffas/recetcd:latest" "PutRange" $delay
       # run "quay.io/coreos/etcd:v3.4.13" "PutSingle" $interval
       # run "jeffas/recetcd:latest" "PutSingle" $interval
       run "quay.io/coreos/etcd:v3.4.13" "PutRandom" $delay
@@ -78,8 +82,8 @@ for node_count in $(seq $min_node_count 2 $max_node_count); do
         run "quay.io/coreos/etcd:v3.4.13" $bencher_subcommand $interval
         run "jeffas/recetcd:latest" $bencher_subcommand $interval
       else
-        run "quay.io/coreos/etcd:v3.4.13" "PutRange" $interval
-        run "jeffas/recetcd:latest" "PutRange" $interval
+        # run "quay.io/coreos/etcd:v3.4.13" "PutRange" $interval
+        # run "jeffas/recetcd:latest" "PutRange" $interval
         # run "quay.io/coreos/etcd:v3.4.13" "PutSingle" $interval
         # run "jeffas/recetcd:latest" "PutSingle" $interval
         run "quay.io/coreos/etcd:v3.4.13" "PutRandom" $interval
