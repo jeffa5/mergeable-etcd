d454c33ae5418a263480493891ecc7849c018740
Andrew Jeffery <dev@jeffas.io>
2021-11-18 11:34:41 +0000

    Update delays

--- experiments/distributed-bencher/meta-run.sh
+++ experiments/distributed-bencher/meta-run.sh
@@ -2,7 +2,7 @@
 
 ansible-playbook setup.yaml
 
-for delay in 1000000 500000 200000 100000 50000 20000 10000; do
+for delay in 200000 100000 66667 50000 40000 33333 28571 25000; do
     while [[ true ]]; do
         ./run.sh -d $delay $@
         if [[ $? -eq 0 ]]; then
