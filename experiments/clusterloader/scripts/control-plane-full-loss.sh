d23d9309c7da2c736bd780e2af7a0bcecd527193
Andrew Jeffery <dev@jeffas.io>
2022-01-26 13:28:11 +0000

    Partition from other nodes (one way)

--- experiments/clusterloader/scripts/control-plane-full-loss.sh
+++ experiments/clusterloader/scripts/control-plane-full-loss.sh
@@ -25,9 +25,21 @@ if [[ -z $target_node ]]; then
     exit 1
 fi
 
+node_ips=$(kubectl get nodes -o json | jq -r ".items[] | select(.status.addresses[1].address != \"$target_node\").status.addresses[0].address")
+
 set +x
 echo "Partitioning $target_node"
-out=$(docker exec $target_node tc qdisc add dev eth0 root netem loss 100% 2>&1)
+out=$(docker exec $target_node tc qdisc add dev eth0 root handle 1: prio 2>&1)
 if [[ $out =~ "Exclusivity flag on, cannot modify." ]]; then
-    docker exec $target_node tc qdisc change dev eth0 root netem loss 100%
+    echo $out
+    exit 1
+else
+    docker exec $target_node tc qdisc add dev eth0 parent 1:1 handle 2: netem loss 100%
+    # handle number
+    h=55
+    for node_ip in $node_ips; do
+        echo "filtering $node_ip"
+        docker exec $target_node tc filter add dev eth0 parent 1:0 protocol ip pref 55 handle ::$h u32 match ip dst $node_ip flowid 2:1
+        h=$((h+1))
+    done
 fi
