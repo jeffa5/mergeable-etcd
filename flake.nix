1e1e5474d969c1ad319d90e74206ce0f8b3dfc7c
Andrew Jeffery <dev@jeffas.io>
2022-10-28 23:20:22 +0100

    Update flake to use crane and remove crate2nix

--- flake.nix
+++ flake.nix
@@ -5,13 +5,18 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     rust-overlay.url = "github:oxalica/rust-overlay";
     rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
+    rust-overlay.inputs.flake-utils.follows = "flake-utils";
     flake-utils.url = "github:numtide/flake-utils";
     perf-tests.url = "github:jeffa5/perf-tests";
     perf-tests.inputs.nixpkgs.follows = "nixpkgs";
+    perf-tests.inputs.flake-utils.follows = "flake-utils";
     kind.url = "github:jeffa5/kind/dev";
     kind.inputs.nixpkgs.follows = "nixpkgs";
+    kind.inputs.flake-utils.follows = "flake-utils";
     crane.url = "github:ipetkov/crane";
     crane.inputs.nixpkgs.follows = "nixpkgs";
+    crane.inputs.rust-overlay.follows = "rust-overlay";
+    crane.inputs.flake-utils.follows = "flake-utils";
   };
 
   outputs = {
@@ -22,249 +27,212 @@
     perf-tests,
     kind,
     crane,
-  }:
-    flake-utils.lib.eachDefaultSystem
-    (
-      system: let
-        pkgs =
-          import nixpkgs
-          {
-            overlays = [(import rust-overlay)];
-            system = system;
-          };
-        lib = pkgs.lib;
-        rust = pkgs.rust-bin.stable.latest.default;
-        buildRustCrate = pkgs.buildRustCrate.override {rustc = rust;};
-        makeCargoNix = release:
-          import ./Cargo.nix {
-            inherit pkgs release;
-            buildRustCrateForPkgs = pkg: buildRustCrate;
-
-            defaultCrateOverrides =
-              pkgs.defaultCrateOverrides
-              // {
-                etcd-proto = attrs: {
-                  buildInputs = [pkgs.protobuf pkgs.rustfmt];
-                  PROTOC = "${pkgs.protobuf}/bin/protoc";
-                };
-                peer-proto = attrs: {
-                  buildInputs = [pkgs.protobuf pkgs.rustfmt];
-                  PROTOC = "${pkgs.protobuf}/bin/protoc";
-                };
-                kubernetes-proto = attrs: {
-                  buildInputs = [pkgs.protobuf pkgs.rustfmt];
-                  PROTOC = "${pkgs.protobuf}/bin/protoc";
-                };
-                prost-build = attrs: {
-                  PROTOC = "${pkgs.protobuf}/bin/protoc";
-                };
-                expat-sys = attrs: {
-                  buildInputs = [pkgs.expat pkgs.pkg-config];
-                };
-                servo-freetype-sys = attrs: {
-                  buildInputs = [pkgs.freetype pkgs.pkg-config];
-                };
-                servo-fontconfig-sys = attrs: {
-                  buildInputs = [pkgs.fontconfig pkgs.pkg-config];
-                };
-              };
-          };
-        cargoNix = makeCargoNix true;
-        debugCargoNix = makeCargoNix false;
-        craneLib = crane.lib.${system};
-        commonArgs = {
-          src = craneLib.cleanCargoSource ./.;
-
-          buildInputs = with pkgs; [
-            fontconfig
-            openssl
-          ];
-
-          nativeBuildInputs = with pkgs; [
-            pkg-config
-            cmake
-          ];
-        };
-        workspaceArgs = let
-          protoFilter = path: type: null != (builtins.match "^.+\\.proto$" path);
-          protoOrCargo = path: type: (protoFilter path type) || (craneLib.filterCargoSources path type);
-        in
-          commonArgs
-          // {
-            src = pkgs.lib.cleanSourceWith {
-              src = ./.;
-              filter = protoOrCargo;
-            };
-            buildInputs = with pkgs; commonArgs.buildInputs ++ [protobuf];
-            PROTOC = "${pkgs.protobuf}/bin/protoc";
-          };
-        cargoArtifacts = craneLib.buildDepsOnly (commonArgs
-          // {
-            pname = "mergeable-etcd-deps";
-          });
-        clippy = craneLib.cargoClippy (workspaceArgs
-          // {
-            inherit cargoArtifacts;
-            cargoClippyExtraArgs = "--all-targets";
-          });
-        metcd = craneLib.buildPackage (workspaceArgs
-          // {
-            inherit cargoArtifacts;
-          });
-        coverage = craneLib.cargoTarpaulin (workspaceArgs
-          // {
-            inherit cargoArtifacts;
-          });
-      in rec
+  }: let
+    system = "x86_64-linux";
+    pkgs =
+      import nixpkgs
       {
-        packages =
-          lib.attrsets.mapAttrs (name: value: value.build) cargoNix.workspaceMembers
-          // {
-            bencher-docker = pkgs.dockerTools.buildLayeredImage {
-              name = "jeffas/bencher";
-              tag = "latest";
-              contents = [
-                pkgs.busybox
-                packages.bencher
-              ];
+        overlays = [rust-overlay.overlays.default];
+        system = system;
+      };
+    lib = pkgs.lib;
+    rust = pkgs.rust-bin.stable.latest.default;
+    buildRustCrate = pkgs.buildRustCrate.override {rustc = rust;};
+    craneLib = crane.lib.${system};
+    commonArgs = {
+      src = craneLib.cleanCargoSource ./.;
 
-              config.Cmd = ["/bin/bencher"];
-            };
+      buildInputs = with pkgs; [
+        fontconfig
+        openssl
+      ];
 
-            mergeable-etcd-etcd = pkgs.stdenv.mkDerivation {
-              name = "mergeable-etcd-etcd";
-              src = packages.mergeable-etcd;
-              installPhase = ''
-                mkdir -p $out/bin
-                cp $src/bin/mergeable-etcd $out/bin/etcd
-              '';
-            };
+      nativeBuildInputs = with pkgs; [
+        pkg-config
+        cmake
+      ];
+    };
+    workspaceArgs = let
+      protoFilter = path: type: null != (builtins.match "^.+\\.proto$" path);
+      protoOrCargo = path: type: (protoFilter path type) || (craneLib.filterCargoSources path type);
+    in
+      commonArgs
+      // {
+        src = pkgs.lib.cleanSourceWith {
+          src = ./.;
+          filter = protoOrCargo;
+        };
+        buildInputs = with pkgs; commonArgs.buildInputs ++ [protobuf];
+        PROTOC = "${pkgs.protobuf}/bin/protoc";
+      };
+    cargoArtifacts = craneLib.buildDepsOnly (commonArgs
+      // {
+        pname = "mergeable-etcd-deps";
+      });
+    clippy = craneLib.cargoClippy (workspaceArgs
+      // {
+        inherit cargoArtifacts;
+        cargoClippyExtraArgs = "--all-targets";
+      });
+    metcd = craneLib.buildPackage (workspaceArgs
+      // {
+        inherit cargoArtifacts;
+      });
+    tarpaulin = craneLib.cargoTarpaulin (workspaceArgs
+      // {
+        inherit cargoArtifacts;
+      });
+    bencher = pkgs.stdenv.mkDerivation {
+      name = "bencher";
+      src = metcd;
+      buildPhase = ''
+        mkdir -p $out/bin
+        cp $src/bin/bencher $out/bin/bencher
+      '';
+    };
+  in {
+    packages.${system} = {
+      bencher-docker = pkgs.dockerTools.buildLayeredImage {
+        name = "jeffas/bencher";
+        tag = "latest";
+        contents = [
+          pkgs.busybox
+          self.packages.${system}.bencher
+        ];
 
-            mergeable-etcd-docker = pkgs.dockerTools.buildLayeredImage {
-              name = "jeffas/mergeable-etcd";
-              tag = "latest";
-              contents = [
-                pkgs.busybox
-                packages.mergeable-etcd
-              ];
-              config.Cmd = ["/bin/mergeable-etcd"];
-            };
+        config.Cmd = ["/bin/bencher"];
+      };
 
-            mergeable-etcd-docker-etcd = pkgs.dockerTools.buildLayeredImage {
-              name = "jeffas/etcd";
-              tag = "latest";
-              contents = [
-                # to allow debugging and using `kubectl cp`
-                pkgs.busybox
-                packages.mergeable-etcd-etcd
-              ];
-              config.Cmd = ["/bin/etcd"];
-            };
+      mergeable-etcd-etcd = pkgs.stdenv.mkDerivation {
+        name = "mergeable-etcd-etcd";
+        src = self.packages.${system}.mergeable-etcd;
+        installPhase = ''
+          mkdir -p $out/bin
+          cp $src/bin/mergeable-etcd $out/bin/etcd
+        '';
+      };
 
-            etcd = pkgs.buildGoModule rec {
-              pname = "etcd";
-              version = "3.4.14";
+      mergeable-etcd-docker = pkgs.dockerTools.buildLayeredImage {
+        name = "jeffas/mergeable-etcd";
+        tag = "latest";
+        contents = [
+          pkgs.busybox
+          self.packages.${system}.mergeable-etcd
+        ];
+        config.Cmd = ["/bin/mergeable-etcd"];
+      };
 
-              src = pkgs.fetchFromGitHub {
-                owner = "etcd-io";
-                repo = "etcd";
-                rev = "v${version}";
-                sha256 = "sha256-LgwJ85UkAQRwpIsILnHDssMw7gXVLO27cU1+5hHj3Wg=";
-              };
+      mergeable-etcd-docker-etcd = pkgs.dockerTools.buildLayeredImage {
+        name = "jeffas/etcd";
+        tag = "latest";
+        contents = [
+          # to allow debugging and using `kubectl cp`
+          pkgs.busybox
+          self.packages.${system}.mergeable-etcd-etcd
+        ];
+        config.Cmd = ["/bin/etcd"];
+      };
 
-              doCheck = false;
+      etcd = pkgs.buildGoModule rec {
+        pname = "etcd";
+        version = "3.4.14";
 
-              deleteVendor = true;
-              vendorSha256 = "sha256-bBlihD5i7YidtVW9Nz1ChU10RE5zjOsXbEL1hA6Blko=";
-            };
-
-            mergeable-etcd-crane = metcd;
-            mergeable-etcd-crane-coverage = coverage;
-            mergeable-etcd-crane-clippy = clippy;
-          };
-
-        defaultPackage = packages.mergeable-etcd;
-
-        apps = {
-          mergeable-etcd = flake-utils.lib.mkApp {
-            name = "mergeable-etcd";
-            drv = packages.mergeable-etcd;
-          };
-
-          experiments = flake-utils.lib.mkApp {
-            name = "experiments";
-            drv = packages.experiments;
-          };
-
-          etcd-benchmark = flake-utils.lib.mkApp {
-            name = "benchmark";
-            drv = packages.etcd;
-          };
-
-          bencher = flake-utils.lib.mkApp {
-            name = "bencher";
-            drv = packages.bencher;
-          };
+        src = pkgs.fetchFromGitHub {
+          owner = "etcd-io";
+          repo = "etcd";
+          rev = "v${version}";
+          sha256 = "sha256-LgwJ85UkAQRwpIsILnHDssMw7gXVLO27cU1+5hHj3Wg=";
         };
 
-        defaultApp = apps.mergeable-etcd;
+        doCheck = false;
 
-        formatter = pkgs.alejandra;
+        deleteVendor = true;
+        vendorSha256 = "sha256-bBlihD5i7YidtVW9Nz1ChU10RE5zjOsXbEL1hA6Blko=";
+      };
 
-        checks =
-          lib.attrsets.mapAttrs
-          (name: value: value.build.override {runTests = true;})
-          debugCargoNix.workspaceMembers;
+      mergeable-etcd = metcd;
+      mergeable-etcd-tarpaulin = tarpaulin;
+      mergeable-etcd-clippy = clippy;
 
-        devShell = pkgs.mkShell {
-          packages = with pkgs;
-            [
-              (rust.override {
-                extensions = ["rust-src" "rustfmt"];
-                targets = ["x86_64-unknown-linux-musl"];
-              })
-              mold
-              cargo-edit
-              cargo-watch
-              cargo-udeps
-              cargo-flamegraph
-              cargo-outdated
-              protobuf
-              kubectl
-              k9s
+      bencher = bencher;
 
-              jupyter
-              python3Packages.numpy
-              python3Packages.pandas
-              python3Packages.matplotlib
-              python3Packages.seaborn
-              python3Packages.isort
-              black
+      default = self.packages.${system}.mergeable-etcd;
+    };
 
-              linuxPackages.perf
+    apps.${system} = {
+      mergeable-etcd = flake-utils.lib.mkApp {
+        name = "mergeable-etcd";
+        drv = self.packages.${system}.mergeable-etcd;
+      };
 
-              cmake
-              pkg-config
-              openssl
-              freetype
-              expat
-              fontconfig
+      experiments = flake-utils.lib.mkApp {
+        name = "experiments";
+        drv = self.packages.${system}.experiments;
+      };
 
-              cfssl
-              etcd
+      etcd-benchmark = flake-utils.lib.mkApp {
+        name = "benchmark";
+        drv = self.packages.${system}.etcd;
+      };
 
-              graphviz
+      bencher = flake-utils.lib.mkApp {
+        name = "bencher";
+        drv = self.packages.${system}.bencher;
+      };
 
-              ansible_2_12
-              python3Packages.ruamel-yaml
-            ]
-            ++ [
-              kind.packages.${system}.kind
-            ];
+      default = self.apps.${system}.mergeable-etcd;
+    };
 
-          ETCDCTL_API = 3;
-          PROTOC = "${pkgs.protobuf}/bin/protoc";
-        };
-      }
-    );
+    formatter.${system} = pkgs.alejandra;
+
+    devShells.${system}.default = pkgs.mkShell {
+      packages = with pkgs;
+        [
+          (rust.override {
+            extensions = ["rust-src" "rustfmt"];
+            targets = ["x86_64-unknown-linux-musl"];
+          })
+          mold
+          cargo-edit
+          cargo-watch
+          cargo-udeps
+          cargo-flamegraph
+          cargo-outdated
+          protobuf
+          kubectl
+          k9s
+
+          jupyter
+          python3Packages.numpy
+          python3Packages.pandas
+          python3Packages.matplotlib
+          python3Packages.seaborn
+          python3Packages.isort
+          black
+
+          linuxPackages.perf
+
+          cmake
+          pkg-config
+          openssl
+          freetype
+          expat
+          fontconfig
+
+          cfssl
+          etcd
+
+          graphviz
+
+          ansible_2_12
+          python3Packages.ruamel-yaml
+        ]
+        ++ [
+          kind.packages.${system}.kind
+        ];
+
+      ETCDCTL_API = 3;
+      PROTOC = "${pkgs.protobuf}/bin/protoc";
+    };
+  };
 }
