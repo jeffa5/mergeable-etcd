da0ff7bc93ce82885a33ad903e8a672f87615682
Andrew Jeffery <dev@jeffas.io>
2022-11-03 07:37:29 +0000

    Run nix fmt

--- experiments/clusterloader/flake.nix
+++ experiments/clusterloader/flake.nix
@@ -6,56 +6,63 @@
     perf-tests.url = "github:jeffa5/perf-tests";
   };
 
-  outputs = { self, nixpkgs, rust-overlay, flake-utils, perf-tests }:
+  outputs = {
+    self,
+    nixpkgs,
+    rust-overlay,
+    flake-utils,
+    perf-tests,
+  }:
     flake-utils.lib.eachDefaultSystem
-      (system:
-        let
-          pkgs = import nixpkgs
-            {
-              overlays = [ (import rust-overlay) ];
-              system = system;
-            };
-          rust = pkgs.rust-bin.stable.latest.default;
-        in
-        {
-          packages = {
-            clusterloader2-docker = pkgs.dockerTools.buildLayeredImage {
-              name = "jeffas/clusterloader2";
-              tag = "kind";
-              contents = perf-tests.packages.${system}.clusterloader2;
-
-              config.Entrypoint = [ "/bin/clusterloader2" "--testconfig=/data/config.yaml" "--provider=kind" "--kubeconfig=/kubeconfig" "--report-dir=/results" "--v=2" ];
-            };
+    (
+      system: let
+        pkgs =
+          import nixpkgs
+          {
+            overlays = [(import rust-overlay)];
+            system = system;
           };
+        rust = pkgs.rust-bin.stable.latest.default;
+      in {
+        packages = {
+          clusterloader2-docker = pkgs.dockerTools.buildLayeredImage {
+            name = "jeffas/clusterloader2";
+            tag = "kind";
+            contents = perf-tests.packages.${system}.clusterloader2;
 
-          devShell = pkgs.mkShell
-            {
-              packages = with pkgs;[
+            config.Entrypoint = ["/bin/clusterloader2" "--testconfig=/data/config.yaml" "--provider=kind" "--kubeconfig=/kubeconfig" "--report-dir=/results" "--v=2"];
+          };
+        };
+
+        devShell =
+          pkgs.mkShell
+          {
+            packages = with pkgs; [
               (rust.override {
-                extensions = [ "rust-src" "rustfmt" ];
-                targets = [ "x86_64-unknown-linux-musl" ];
+                extensions = ["rust-src" "rustfmt"];
+                targets = ["x86_64-unknown-linux-musl"];
               })
-                just
-                kind
-                kubectl
-                k9s
-                ansible_2_11
-                kubernetes-helm
-                openssl
+              just
+              kind
+              kubectl
+              k9s
+              ansible_2_11
+              kubernetes-helm
+              openssl
 
-                cargo-edit
+              cargo-edit
 
-                jupyter
-                black
-                python3Packages.seaborn
-                python3Packages.kubernetes
-                python3Packages.docker
+              jupyter
+              black
+              python3Packages.seaborn
+              python3Packages.kubernetes
+              python3Packages.docker
 
-                perf-tests.packages.${system}.clusterloader2
-              ];
+              perf-tests.packages.${system}.clusterloader2
+            ];
 
-              # KUBECONFIG="./admin.conf";
-            };
-        }
-      );
+            # KUBECONFIG="./admin.conf";
+          };
+      }
+    );
 }
