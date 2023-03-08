79e06121e1ee7bc98195033c018f4035530858b7
Andrew Jeffery <dev@jeffas.io>
2023-03-08 18:08:06 +0000

    Remove ecetcd and recetcd

--- ecetcd/src/store/value.rs
+++
@@ -1,5 +0,0 @@
-// mod ivalue;
-mod snapshot;
-
-// pub use ivalue::{IValue, StoreValue, LEASE_ID_KEY, REVISIONS_KEY};
-pub use snapshot::SnapshotValue;
--- ecetcd/src/store/version.rs
+++
@@ -1,7 +0,0 @@
-use std::num::NonZeroU64;
-
-/// The version of a resource
-///
-/// - `None` if the resource has been deleted at the revision
-/// - `Some(n)` otherwise and n will be the number of changes since creation
-pub type Version = Option<NonZeroU64>;
