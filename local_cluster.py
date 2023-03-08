d182e1707ca52403617cc24f87b5f8c4e3d8dce4
Andrew Jeffery <dev@jeffas.io>
2023-03-08 15:55:36 +0000

    Work with client tls connections

--- local_cluster.py
+++ local_cluster.py
@@ -4,15 +4,18 @@ Create a local cluster of mergeable-etcd nodes for easy testing.
 """
 
 import argparse
-import json
 import os
 import shutil
 import subprocess
+from typing import List
 
 BASE_PORT = 2379
+CERTS_DIR = "certs"
 
 
-def spawn_start_node(bin_path: str, workspace: str) -> subprocess.Popen:
+def spawn_start_node(
+    bin_path: str, workspace: str, client_scheme: str, tls: List[str]
+) -> subprocess.Popen:
     """
     Spawn an initial node.
     """
@@ -33,9 +36,9 @@ def spawn_start_node(bin_path: str, workspace: str) -> subprocess.Popen:
         "--log-filter",
         "info",
         "--listen-client-urls",
-        f"http://127.0.0.1:{client_port}",
+        f"{client_scheme}://127.0.0.1:{client_port}",
         "--advertise-client-urls",
-        f"http://127.0.0.1:{client_port}",
+        f"{client_scheme}://127.0.0.1:{client_port}",
         "--listen-peer-urls",
         f"http://127.0.0.1:{peer_port}",
         "--initial-advertise-peer-urls",
@@ -46,14 +49,16 @@ def spawn_start_node(bin_path: str, workspace: str) -> subprocess.Popen:
         "new",
         "--initial-cluster",
         initial_cluster,
-    ]
+    ] + tls
     print("Launching node", cmd)
     out_file = open(os.path.join(workspace, "out"), "w")
     err_file = open(os.path.join(workspace, "err"), "w")
     return subprocess.Popen(cmd, stdout=out_file, stderr=err_file)
 
 
-def spawn_join_node(bin_path: str, workspace: str, index: int) -> subprocess.Popen:
+def spawn_join_node(
+    bin_path: str, workspace: str, index: int, client_scheme: str, tls: List[str]
+) -> subprocess.Popen:
     """
     Spawn a join node.
     """
@@ -64,9 +69,9 @@ def spawn_join_node(bin_path: str, workspace: str, index: int) -> subprocess.Pop
     client_port = BASE_PORT + (10 * index)
     peer_port = BASE_PORT + 1 + (10 * index)
     metrics_port = BASE_PORT + 2 + (10 * index)
-    initial_cluster = ",".join([
-        f"node{i}=http://127.0.0.1:{BASE_PORT + 1 + (10 * i)}" for i in range(index)
-    ])
+    initial_cluster = ",".join(
+        [f"node{i}=http://127.0.0.1:{BASE_PORT + 1 + (10 * i)}" for i in range(index)]
+    )
     cmd = [
         bin_path,
         "--name",
@@ -76,9 +81,9 @@ def spawn_join_node(bin_path: str, workspace: str, index: int) -> subprocess.Pop
         "--log-filter",
         "info",
         "--listen-client-urls",
-        f"http://127.0.0.1:{client_port}",
+        f"{client_scheme}://127.0.0.1:{client_port}",
         "--advertise-client-urls",
-        f"http://127.0.0.1:{client_port}",
+        f"{client_scheme}://127.0.0.1:{client_port}",
         "--listen-peer-urls",
         f"http://127.0.0.1:{peer_port}",
         "--initial-advertise-peer-urls",
@@ -89,14 +94,14 @@ def spawn_join_node(bin_path: str, workspace: str, index: int) -> subprocess.Pop
         "existing",
         "--initial-cluster",
         initial_cluster,
-    ]
+    ] + tls
     print("Launching node", cmd)
     out_file = open(os.path.join(workspace, "out"), "w")
     err_file = open(os.path.join(workspace, "err"), "w")
     return subprocess.Popen(cmd, stdout=out_file, stderr=err_file)
 
 
-def add_node(index: int):
+def add_node(index: int, tls: List[str]):
     """
     Add node to the cluster configuration before it starts.
     """
@@ -110,7 +115,7 @@ def add_node(index: int):
         f"http://127.0.0.1:{peer_port}",
         "-w",
         "json",
-    ]
+    ] + tls
     print("Command", cmd)
     member_add = subprocess.run(
         cmd,
@@ -129,15 +134,33 @@ def spawn_cluster(args: argparse.Namespace):
     bin_name = "mergeable-etcd"
     build_type = "debug"
     bin_path = os.path.join("target", build_type, bin_name)
+    client_scheme = "https" if args.client_tls else "http"
+    tls = []
+    if args.client_tls:
+        tls += [
+            "--cert-file",
+            os.path.join("certs", "server.crt"),
+            "--key-file",
+            os.path.join("certs", "server.key"),
+        ]
+
     nodes = []
     node_dir = os.path.join(args.workspace, "node0")
-    node = spawn_start_node(bin_path, node_dir)
+    node = spawn_start_node(bin_path, node_dir, client_scheme, tls)
     nodes.append(node)
 
     for index in range(1, args.nodes):
         node_dir = os.path.join(args.workspace, f"node{index}")
-        add_node(index)
-        node = spawn_join_node(bin_path, node_dir, index)
+        add_tls = []
+        if args.client_tls:
+            add_tls += [
+                "--cacert",
+                os.path.join("certs", "ca.pem"),
+                "--endpoints",
+                "https://127.0.0.1:2379",
+            ]
+        add_node(index, add_tls)
+        node = spawn_join_node(bin_path, node_dir, index, client_scheme, tls)
         nodes.append(node)
 
     input("Press Enter to stop the cluster...")
@@ -155,7 +178,12 @@ def main():
     parser.add_argument(
         "--nodes", type=int, default="1", help="Number of nodes to spawn"
     )
-    # parser.add_argument("--client-tls", type=bool, help="Whether to launch nodes with client tls connections")
+    parser.add_argument(
+        "--client-tls",
+        action="store_true",
+        default=False,
+        help="Whether to launch nodes with client tls connections",
+    )
     # parser.add_argument("--peer-tls", type=bool, help="Whether to launch nodes with peer tls connections")
     args = parser.parse_args()
     print(args)
