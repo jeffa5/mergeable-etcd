09a39678c8b671cab0a1d28c44d77bd197460b64
Andrew Jeffery <dev@jeffas.io>
2023-03-08 16:47:27 +0000

    Cleanup custom tls channel from initial_cluster test

--- crates/mergeable-etcd/tests/initial_cluster.rs
+++ crates/mergeable-etcd/tests/initial_cluster.rs
@@ -1,29 +1,18 @@
 use etcd_proto::etcdserverpb::cluster_client::ClusterClient;
 use etcd_proto::etcdserverpb::kv_client::KvClient;
-use hyper::client::HttpConnector;
-use hyper::client::ResponseFuture;
-use hyper::Body;
-use hyper::Client;
-use hyper::Request;
-use hyper::Response;
+
 use hyper::Uri;
-use hyper_openssl::HttpsConnector;
-use openssl::ssl::SslConnector;
-use openssl::ssl::SslMethod;
-use openssl::x509::X509;
 use pretty_assertions::assert_eq;
 use reqwest::StatusCode;
-use std::error::Error;
-use std::task::Poll;
+use tonic::transport::Certificate;
+use tonic::transport::Channel;
+use tonic::transport::ClientTlsConfig;
 use std::{
     sync::atomic::{AtomicU32, Ordering},
     time::Duration,
 };
 use tempdir::TempDir;
 use test_log::test;
-use tonic::body::BoxBody;
-use tonic_openssl::ALPN_H2_WIRE;
-use tower::Service;
 use tracing::info;
 
 use etcd_proto::etcdserverpb::{MemberAddRequest, PutRequest, RangeRequest, RangeResponse};
@@ -87,11 +76,11 @@ async fn make_cluster(
     tls: bool,
 ) -> (
     Vec<TempDir>,
-    Vec<(KvClient<MyChannel>, ClusterClient<MyChannel>)>,
+    Vec<(KvClient<Channel>, ClusterClient<Channel>)>,
 ) {
     let addresses = get_addresses(nodes, tls);
 
-    let mut clients: Vec<(KvClient<MyChannel>, ClusterClient<MyChannel>)> = Vec::new();
+    let mut clients: Vec<(KvClient<Channel>, ClusterClient<Channel>)> = Vec::new();
     let mut tempdirs = Vec::new();
 
     let mut initial_cluster = Vec::new();
@@ -159,7 +148,7 @@ async fn make_cluster(
     (tempdirs, clients)
 }
 
-async fn get_kv_client(address: &str) -> KvClient<MyChannel> {
+async fn get_kv_client(address: &str) -> KvClient<Channel> {
     let uri = address.parse::<Uri>().unwrap();
     let ca_file = if uri.scheme_str() == Some("https") {
         Some(CA_FILE)
@@ -170,7 +159,7 @@ async fn get_kv_client(address: &str) -> KvClient<MyChannel> {
     KvClient::new(channel)
 }
 
-async fn get_cluster_client(address: &str) -> ClusterClient<MyChannel> {
+async fn get_cluster_client(address: &str) -> ClusterClient<Channel> {
     let uri = address.parse::<Uri>().unwrap();
     let ca_file = if uri.scheme_str() == Some("https") {
         Some(CA_FILE)
@@ -181,75 +170,13 @@ async fn get_cluster_client(address: &str) -> ClusterClient<MyChannel> {
     ClusterClient::new(channel)
 }
 
-async fn get_channel(ca_file: Option<&str>, uri: Uri) -> MyChannel {
-    let pem = if let Some(ca_file) = ca_file {
-        Some(tokio::fs::read(ca_file).await.unwrap())
-    } else {
-        None
+async fn get_channel(ca_file: Option<&str>, uri: Uri) -> Channel {
+    let mut channel = Channel::builder(uri);
+    if let Some(ca_file) = ca_file {
+        let pem = tokio::fs::read(ca_file).await.unwrap();
+        channel = channel.tls_config(ClientTlsConfig::new().ca_certificate(Certificate::from_pem(pem))).unwrap();
     };
-    MyChannel::new(pem, uri).await.unwrap()
-}
-
-#[derive(Clone)]
-pub struct MyChannel {
-    uri: Uri,
-    client: MyClient,
-}
-
-#[derive(Clone)]
-enum MyClient {
-    ClearText(Client<HttpConnector, BoxBody>),
-    Tls(Client<HttpsConnector<HttpConnector>, BoxBody>),
-}
-
-impl MyChannel {
-    pub async fn new(ca_certificate: Option<Vec<u8>>, uri: Uri) -> Result<Self, Box<dyn Error>> {
-        let mut http = HttpConnector::new();
-        http.enforce_http(false);
-        let client = match ca_certificate {
-            None => MyClient::ClearText(Client::builder().http2_only(true).build(http)),
-            Some(pem) => {
-                let ca = X509::from_pem(&pem[..])?;
-                let mut connector = SslConnector::builder(SslMethod::tls())?;
-                connector.cert_store_mut().add_cert(ca)?;
-                connector.set_alpn_protos(ALPN_H2_WIRE)?;
-                let mut https = HttpsConnector::with_connector(http, connector)?;
-                https.set_callback(|c, _| {
-                    c.set_verify_hostname(false);
-                    Ok(())
-                });
-                MyClient::Tls(Client::builder().http2_only(true).build(https))
-            }
-        };
-
-        Ok(Self { client, uri })
-    }
-}
-
-// Check out this blog post for an introduction to Tower:
-// https://tokio.rs/blog/2021-05-14-inventing-the-service-trait
-impl Service<Request<BoxBody>> for MyChannel {
-    type Response = Response<Body>;
-    type Error = hyper::Error;
-    type Future = ResponseFuture;
-
-    fn poll_ready(&mut self, _: &mut std::task::Context<'_>) -> Poll<Result<(), Self::Error>> {
-        Ok(()).into()
-    }
-
-    fn call(&mut self, mut req: Request<BoxBody>) -> Self::Future {
-        let uri = Uri::builder()
-            .scheme(self.uri.scheme().unwrap().clone())
-            .authority(self.uri.authority().unwrap().clone())
-            .path_and_query(req.uri().path_and_query().unwrap().clone())
-            .build()
-            .unwrap();
-        *req.uri_mut() = uri;
-        match &self.client {
-            MyClient::ClearText(client) => client.request(req),
-            MyClient::Tls(client) => client.request(req),
-        }
-    }
+    channel.connect().await.unwrap()
 }
 
 #[test(tokio::test)]
