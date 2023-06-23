5f74ac8f8db6544fb22d9d5ce9e7d201dc8e92fa
Andrew Jeffery <dev@jeffas.io>
2023-06-23 16:49:26 +0000

    Update startn script

--- scripts/cluster/startn.sh
+++ scripts/cluster/startn.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
 bin=${1:-mergeable-etcd-bytes}
+size=${2:-3}
 
-for n in {2..9}; do
+for n in $(seq 2 $size); do
   echo $n
-  client_port=$((2379 + 3*$n))
-  peer_port=$((2380 + 3*$n))
-  metrics_port=$((2381 + 3*$n))
+  client_port=$((2379 + 10*$n))
+  peer_port=$((2380 + 10*$n))
+  metrics_port=$((2381 + 10*$n))
   cargo run --bin $bin --release -- --initial-cluster-state existing --persister memory --name node$n --initial-cluster "node1=http://127.0.0.1:2380" --listen-client-urls "http://127.0.0.1:$client_port" --listen-peer-urls "http://127.0.0.1:$peer_port" --initial-advertise-peer-urls "http://127.0.0.1:$peer_port" --listen-metrics-urls "http://127.0.0.1:$metrics_port" &
 done
 
