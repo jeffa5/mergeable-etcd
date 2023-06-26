f0db2e057c3448bcf5f12e9070e9c607ef4ca93f
Andrew Jeffery <dev@jeffas.io>
2023-06-26 17:22:12 +0100

    Remove old experiments

--- experiments/clusterloader/scripts/show-tc.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-nodes=$(kubectl get nodes -o name)
-
-for node in $nodes; do
-    node=${node##*/}
-    echo $node
-    docker exec $node tc qdisc show
-done
--- experiments/distributed-bencher/teardown.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-ansible-playbook main.yaml -e @values.yaml --tags teardown -e node_count=21
